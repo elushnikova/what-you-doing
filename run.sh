7abdab4eff8d7c474b24981977bbf935baf2e1ff
Lena Lushnikova <git@elushnikova.dev>
2023-10-05 16:15:58 +0300

    Allow starting the tool using absolute path

--- run.sh
+++ run.sh
@@ -1,10 +1,11 @@
 #!/bin/zsh
 
 WEEK_DAY=$(LC_TIME=en_US.UTF-8 date +'%a')
-RESPONSE_FILE="./responses/$(date +'%Y-%m-%d').${WEEK_DAY:l}.txt"
+WORKING_DIR=$(dirname "$0")
+RESPONSE_FILE="${WORKING_DIR}/responses/$(date +'%Y-%m-%d').${WEEK_DAY:l}.txt"
 
 # Run AppleScript and save response
-response=$(osascript ./ui.applescript)
+response=$(osascript "${WORKING_DIR}/ui.applescript")
 
 # Record the response along with a timestamp
 echo "$(date): $response" >>$RESPONSE_FILE
